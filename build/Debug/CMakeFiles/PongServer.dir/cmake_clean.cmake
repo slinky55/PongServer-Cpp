0e6a09601fd4654f5249c7f0736763165f967edb
slinky55 <larry.mason@ufl.edu>
2022-09-08 10:42:59 -0400

    initial commit

---
+++ build/Debug/CMakeFiles/PongServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PongServer.dir/main.cpp.o"
+  "CMakeFiles/PongServer.dir/main.cpp.o.d"
+  "PongServer"
+  "PongServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PongServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SFML/Network.hpp>
+
+int main()
+{
+	std::cout << "Starting Server...\n";
+	return 0;
+}
